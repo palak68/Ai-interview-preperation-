16e17fecaa895a646eaa59f3eb19e8cee2afdd01
Palak Sharma <siyamukeshsharma@gmail.com>
2026-06-27 23:09:34 +0530

    Add client and server files

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="w-full min-h-screen bg-[#f3f3f3]
+     flex items-center justify-center px-6 py-12 lg:px-8">
+    <div className="w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl 
+    border border-gray-200">
+    <div className="flex items-center justify-center gap-3 mb-6"> </div>
+        <div className="bg-black text-white p-2 rounded-lg"></div>
+        <h2>Ai interview preperation</h2>
+   </div>
+    </div>
+  );
+};
+
+export default Auth;
