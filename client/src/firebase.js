d927dc00b9a297a3c27a8c123071902def13aa71
Palak Sharma <siyamukeshsharma@gmail.com>
2026-06-29 23:04:37 +0530

    update firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBT-eiaBmTyUbvKSJLL2LhsmKEavmJJW1c",
+  authDomain: "ai-interview-preperation-1fe58.firebaseapp.com",
+  projectId: "ai-interview-preperation-1fe58",
+  storageBucket: "ai-interview-preperation-1fe58.firebasestorage.app",
+  messagingSenderId: "867380513564",
+  appId: "1:867380513564:web:d846f04ef459fde5b288d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
